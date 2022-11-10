3bafbce88d2fe120098d8db3363c1d9ef486a50e
Jesus Toquinto <14854738+jitoquinto@users.noreply.github.com>
2022-11-10 09:58:58 -0600

    fix: daily stats can be none, add default value

--- source_stackadapt/streams.py
+++ source_stackadapt/streams.py
@@ -376,7 +376,8 @@ class IncrementalStackadaptStatStream(StackadaptStream):
         stats_response = response.json()
 
         # Yield from response only if there is available data
-        daily_stats = stats_response.get(self.TIME_BASED_STATS_KEY, [])
+        daily_stats = stats_response.get(self.TIME_BASED_STATS_KEY)
+        daily_stats = daily_stats if daily_stats else []
         logger.info(f"Retrieved {len(daily_stats)} {self.GROUP_BY_RESOURCE} stat record(s)")
         yield from daily_stats if daily_stats else []
 
